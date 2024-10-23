b31cda20d3d2ca61a7ac98f6e46243d7f91d19ca
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-22 20:06:07 -0500

    Create push_&_pop.js

---
+++ Arrays/push_&_pop.js
@@ -0,0 +1,16 @@
+Push Method
+Adds one or more elements to the end of the array.
+
+  let dailyActivities = ["eat", "sleep"];
+dailyActivities.push("exercise");
+console.log(dailyActivities); // ["eat", "sleep", "exercise"]
+
+
+Pop Method
+Removes the last element from the array and returns it.
+
+  let fruits = ["apple", "banana", "cherry"];
+let lastFruit = fruits.pop();
+console.log(fruits); // ["apple", "banana"]
+console.log(lastFruit); // "cherry"
+
