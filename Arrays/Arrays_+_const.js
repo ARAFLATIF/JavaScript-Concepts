067aabba5a186b5e62017bb2bcbeb18c4fcd20d2
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-23 01:52:51 -0500

    Create Arrays_+_const.js

---
+++ Arrays/Arrays_+_const.js
@@ -0,0 +1,8 @@
+Using Const with Arrays
+Declaring an array with const does not make the array immutable; it only prevents reassignment of the variable.
+
+
+  const colors = ["red", "green", "blue"];
+colors.push("yellow");
+console.log(colors); // ["red", "green", "blue", "yellow"]
+
