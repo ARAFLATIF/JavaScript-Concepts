b000568c432e6af582528eb84a188b0ebb336029
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-23 01:57:57 -0500

    Update Introduction.js

--- Object-Literals/Introduction.js
+++ Object-Literals/Introduction.js
@@ -1,7 +1,19 @@
-Using Const with Arrays
-Declaring an array with const does not make the array immutable; it only prevents reassignment of the variable.
+What are Object Literals?
+An object literal is a way to create an object in JavaScript using a concise syntax. It is defined by a set of key-value pairs enclosed in curly braces {}.
 
-  const colors = ["red", "green", "blue"];
-colors.push("yellow");
-console.log(colors); // ["red", "green", "blue", "yellow"]
+
+const person = { name: 'John', age: 30, occupation: 'Developer' };
+
+
+Key Features
+Properties: Objects can have properties, which are key-value pairs.
+Methods: Objects can also have methods, which are functions that belong to the object.
+
+  const person = {
+  name: 'John',
+  age: 30,
+  occupation: 'Developer',
+  greet: function() { console.log('Hello!'); }
+};
+
 
