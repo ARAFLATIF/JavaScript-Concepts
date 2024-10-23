5cec12ce6af6e875b6aa1100ccf030640d02e8c1
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-22 20:04:33 -0500

    Create intro.js

---
+++ Arrays/intro.js
@@ -0,0 +1,30 @@
+What are Arrays?
+An array is a collection of elements of any data type (strings, numbers, booleans, objects) stored in a single variable.
+Example:
+
+let colors = ["red", "green", "blue"];
+
+
+Creating Arrays
+Array Literal:
+
+let colors = ["red", "green", "blue"];
+
+
+Array Constructor :
+
+let colors = new Array("red", "green", "blue");
+
+
+Accessing Elements
+Elements are accessed using their index, which starts at 0.
+
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits[0]); // "apple"
+
+Array Length
+The length property returns the number of elements in an array.
+
+  let fruits = ["apple", "banana", "cherry"];
+console.log(fruits.length); // 3
+
