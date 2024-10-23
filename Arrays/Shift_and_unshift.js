b4c386ac0f484de099af9b413638536cb6907026
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-22 20:07:16 -0500

    Create Shift_and_unshift.js

---
+++ Arrays/Shift_and_unshift.js
@@ -0,0 +1,16 @@
+Shift Method
+Removes the first element from the array and returns it.
+
+  let fruits = ["apple", "banana", "cherry"];
+let firstFruit = fruits.shift();
+console.log(fruits); // ["banana", "cherry"]
+console.log(firstFruit); // "apple"
+
+
+Unshift Method
+Adds one or more elements to the beginning of the array.
+
+  let dailyActivities = ["eat", "sleep"];
+dailyActivities.unshift("exercise");
+console.log(dailyActivities); // ["exercise", "eat", "sleep"]
+
