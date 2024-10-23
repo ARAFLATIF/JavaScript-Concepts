e640d09955ebaa3241131feae1ba8d93ce945d1c
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-23 12:49:31 -0500

    Create for_loop.js

---
+++ Loops/for_loop.js
@@ -0,0 +1,53 @@
+What are For Loops?
+A for loop is a control flow statement that allows code to be executed repeatedly for a specified number of iterations. It typically involves an initialization, a test expression, and an update of the loop variable.
+
+
+  Example: Basic For Loop
+
+for (let count = 0; count < 5; count++) {
+    console.log(count);
+}
+
+Output :
+
+0
+1
+2
+3
+4
+
+
+
+More For Loops Examples
+Counting up by one:
+
+for (let count = 0; count <= 5; count++) {
+    console.log(count);
+}
+
+Output :
+
+0
+1
+2
+3
+4
+5
+
+
+
+Counting up in even numbers:
+
+for (let count = 0; count <= 10; count += 2) {
+    console.log(count);
+}
+
+Output : 
+
+0
+2
+4
+6
+8
+10
+
