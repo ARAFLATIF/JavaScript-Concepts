7394308003306e075cc06b33dc9b373729928180
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-22 20:09:46 -0500

    Create Concat_Indexof_includes_reverse.js

---
+++ Arrays/Concat_Indexof_includes_reverse.js
@@ -0,0 +1,31 @@
+Concat Method
+Merges two or more arrays into a new array.
+
+  let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let merged = arr1.concat(arr2);
+console.log(merged); // [1, 2, 3, 4, 5, 6]
+
+
+IndexOf Method
+Returns the index of the first occurrence of a specified value in an array.
+
+  let fruits = ["apple", "banana", "cherry"];
+console.log(fruits.indexOf("banana")); // 1
+
+
+
+Includes Method
+Checks if an array includes a certain value and returns a boolean.
+
+
+  let fruits = ["apple", "banana", "cherry"];
+console.log(fruits.includes("banana")); // true
+
+
+Reverse Method
+Reverses the order of elements in an array
+
+let numbers = [1, 2, 3, 4];
+numbers.reverse();
+console.log(numbers); // [4, 3, 2, 1]
