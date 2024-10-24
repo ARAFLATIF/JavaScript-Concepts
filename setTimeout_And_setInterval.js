abb16192efb3c202ab399ec7911c4e30eba2787c
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-24 02:45:10 -0500

    Create setTimeout_And_setInterval.js

---
+++ setTimeout_And_setInterval.js
@@ -0,0 +1,17 @@
+Overview
+setTimeout and setInterval are methods used to execute functions after a specified amount of time or at regular intervals.
+setTimeout
+The setTimeout method executes a function once after a specified delay.
+
+  setTimeout(function() {
+    console.log("This will be executed after 2 seconds");
+}, 2000);
+
+
+setInterval
+The setInterval method executes a function at regular intervals.
+
+  setInterval(function() {
+    console.log("This will be executed every 2 seconds");
+}, 2000);
+
