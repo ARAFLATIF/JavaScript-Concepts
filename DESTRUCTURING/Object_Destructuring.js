3234a48b85474f0d727fd95feffc58c4a6eaaf24
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-24 14:32:25 -0500

    Update Object_Destructuring.js

--- DESTRUCTURING/Object_Destructuring.js
+++ DESTRUCTURING/Object_Destructuring.js
@@ -32,32 +32,3 @@ const { address: { city } } = user;
 console.log(city); // Output: New York
 
 
-Destructuring Function Parameters
-Destructuring can also be used directly in function parameters to extract values from objects or arrays passed as arguments.
-
-
-  EXAMPLE WITH OBJECTS : 
-
-function greet({ name, age }) {
-    console.log(`Hello, ${name}. You are ${age} years old.`);
-}
-
-greet({ name: "Peter", age: 50 });
-// Output: Hello, Peter. You are 50 years old.
-
-
-
-EXAMPLE WITH ARRAYS:
-
-function sum([a, b]) {
-    return a + b;
-}
-
-console.log(sum([5, 10])); // Output: 15
-
-
-
-Destructuring simplifies the process of extracting values from arrays and objects, 
-  making code more readable and concise. It is particularly useful when dealing with complex data structures 
-  or when you only need specific parts of an object or array.
-
